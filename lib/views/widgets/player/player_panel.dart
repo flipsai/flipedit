ef1d4489279ffd4b4dc446119a9792befe7253c7
Rémy Menard <remymenard.work@gmail.com>
2025-05-16 19:20:05 +0200

    remove old player

--- lib/views/widgets/player/player_panel.dart
+++ lib/views/widgets/player/player_panel.dart
@@ -1,10 +1,5 @@
-import 'package:flipedit/views/widgets/player/player.dart';
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:flutter/material.dart' as material;
-import 'package:watch_it/watch_it.dart';
 import 'package:flipedit/utils/logger.dart';
-import 'package:flipedit/viewmodels/timeline_navigation_viewmodel.dart';
-import 'package:flipedit/viewmodels/player/native_player_viewmodel.dart';
 
 class PlayerPanel extends StatefulWidget {
   const PlayerPanel({super.key});
@@ -27,20 +22,7 @@ class _PlayerPanelState extends State<PlayerPanel> {
   @override
   Widget build(BuildContext context) {
     logDebug("Rebuilding PlayerPanel...", 'PlayerPanel');
-
-    Widget content = Container(
-            color: const Color(0xFF333333),
-            child: const Stack(
-              children: [
-                Player()
-              ],
-            ),
-          );
-
-        return material.Material(
-          color: const Color(0xFF333333),
-          child: content,
-    );
+    return Container();
   }
 }
 
